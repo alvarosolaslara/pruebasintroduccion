05c978bec7545460b795b880b4d8ea568e3658ca
alvarosolaslara <43420050+alvarosolaslara@users.noreply.github.com>
2026-02-03 17:08:24 +0100

    ultimo

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView #lo importamos de views.py
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView, ejemplo #lo importamos de views.py
 
 app_name="blog"
 
@@ -7,5 +7,13 @@ urlpatterns=[# accederemos a todas las vistas básicas de nuestro blog
 
     path('', BlogListView.as_view(), name='home'), # con esto conectamos a las url de core, este es el home
     path('create/', BlogCreateView.as_view(), name='create'),
+
+    path('update/', BlogUpdateView.as_view(), name='update'),
+    
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='delete'),# <int:pk> hace referencia al id de cada modelo, en este caso es importante para saber que es lo que estamos borrando
+
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail'), # <int:pk> hace referencia al id de cada modelo
+
+    path('movimiento/', ejemplo.as_view(), name='mov'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import View, UpdateView, DeleteView #CON UPDATE MODIFICAREMOS UNA VISTA
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy #para redirigir 
 
 
 class BlogListView(View): # creamos las vistas de blog, aquí enlistaremos todos los post que existen
     def get(self, request, *args, **kwargs):
+        #Para enlistar llamaremos a los POST (a sus objetos) que se encuentran en la base de datos
+        posts = Post.objects.all() #para ver esta información la pasaremos al contexto para luegos hacer referencia en el html
         context={
-
+            'posts':posts
         }
         return render(request, 'blog_list.html', context)
     
@@ -21,9 +24,9 @@ class BlogCreateView(View):
         return render(request, 'blog_create.html', context)
     
     def post(self, request, *args, **kwargs):
-        if request.method=="POST":
+        if request.method=="POST": # si el request es igual a POST, le pasaremos la información que se encuentra dentro de FORM
             form=PostCreateForm(request.POST)
-            if form.is_valid():
+            if form.is_valid(): #si el formulario es válido tendremos la información, título y contenido
                 title = form.cleaned_data.get('title')
                 content = form.cleaned_data.get('content')
                 p, created = Post.objects.get_or_create(title=title, content=content)
@@ -33,3 +36,38 @@ class BlogCreateView(View):
 
         }
         return render(request, 'blog_create.html', context)
+    
+
+
+class BlogDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context={
+            'post':post
+        }
+        return render(request, 'blog_detail.html',context)
+    
+class BlogUpdateView(UpdateView):
+    model=Post #Ahora le pasaremos los campos que queremos editar
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    #cuando se actualice se nor redirigirá al enlace que indicaremos a continuación
+    def get_success_url(self):
+        pk = self.kwargs['pk'] #información del objeto que se encuentra dentro llamando, es decir, del modelo arriba llamado POST
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+
+
+class BlogDeleteView(DeleteView):
+    model=Post #Se le pasa a Delete view el model que se dea borrar
+    template_name='blog_delete.html'
+    success_url= reverse_lazy('blog:home')
+
+
+    
+class ejemplo(View):
+    def get(self, request, *args, **kwargs):
+        context={
+
+        }
+        return render(request, 'vision.html', context)
